3ff828d5f35559520d26b0b02c8ea736cfed2dcb
hooriehghahremani <71557918+hooriehghahremani@users.noreply.github.com>
2021-12-23 19:35:16 +0330

    third commit

--- src/test.java
+++ src/test.java
@@ -2,36 +2,39 @@ package com.company;
 
 import java.util.Scanner;
 
-public class Switch {
+public class test {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String dayOfWeek_String = scanner.nextLine();
-        int dayOfWeek = Integer.parseInt(dayOfWeek_String);
-        switch (dayOfWeek) {
-            case 1: // dayOfWeek == 1
-                System.out.println("Saturday");
-                break;
-            case 2: // dayOfWeek == 2
-                System.out.println("Sunday");
-                break;
-            case 3: // dayOfWeek == 3
-                System.out.println("Monday");
-                break;
-            case 4: // dayOfWeek == 4
-                System.out.println("Thursday");
-                break;
-            case 5: // dayOfWeek == 5
-                System.out.println("Wednesday");
-                break;
-            case 6: // dayOfWeek == 6
-                System.out.println("Tuesday");
-                break;
-            case 7: // dayOfWeek == 7
-                System.out.println("Friday");
-                break;
-            default: // not handled
-                System.out.println("please inter current number");
-                break;
+        int dayOfWeek_String = scanner.nextLine();
+
+        if (dayOfWeek ==1 )
+        {
+            System.out.println("Saturday");
         }
+        else if (dayOfWeek ==2 )
+        {
+            System.out.println("Sunday");
+        }
+        else if (dayOfWeek ==3 )
+        {
+            System.out.println("Monday");
+        }
+        else if (dayOfWeek ==4 )
+        {
+            System.out.println("Thursday");
+        }
+        else if (dayOfWeek ==5 )
+        {
+            System.out.println("Wednesday");
+        }
+        else if (dayOfWeek ==6 )
+        {
+            System.out.println("Tuesday");
+        }
+        else if (dayOfWeek ==7 )
+        {
+            System.out.println("Friday");
+        }
+
     }
 }
